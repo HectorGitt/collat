bf60b5c27c6bcf815d1c80c50e4b9707bc8b52a9
Hector <adeniyi.olaitanhector@yahoo.com>
2024-04-19 21:18:15 +0100

    Fix height value in NavBar component

--- src/scenes/Nav.jsx
+++ src/scenes/Nav.jsx
@@ -165,7 +165,7 @@ const NavBar = styled.nav`
     top: 0;
     left: 0;
     width: 100%;
-    height: 100vh;
+    height: 100%;
     overflow-y: hidden;
     background: #19202b;
     background-image: linear-gradient(315deg, #19202b 0%, #19202b 74%);
